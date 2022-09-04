5e588d68da3dc1591ad9ddc3c73e89fb2fd06252
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-09-04 10:33:28 -0400

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -22,8 +22,8 @@ class BaseModel():
             created along with the class be default
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
         """It update the class"""
         delim = "%Y-%m-%dT%H:%M:%S.%f"
@@ -49,7 +49,7 @@ class BaseModel():
     def save(self):
         """Update the public instance attribute
         updated_at with the current datetime"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
